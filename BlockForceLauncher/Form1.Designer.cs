6307e73a981af12bcb76d28f7b4b05b80e918e8b
supₐ <102895212+generic-glitch@users.noreply.github.com>
2022-10-09 12:13:46 -0500

    Updated Icon for EXE

--- BlockForceLauncher/Form1.Designer.cs
+++ BlockForceLauncher/Form1.Designer.cs
@@ -152,6 +152,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Block Source Launcher";
